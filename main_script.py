4994ef2e89d3f7ba0186a6536b05f7a2c32b6803
Arch4nge <91314962+Arch4nge@users.noreply.github.com>
2022-01-04 18:59:02 +0100

    Moving variables from main_script into .env file

--- main_script.py
+++ main_script.py
@@ -2,34 +2,22 @@
 
 import os
 
-domain = 'example.com'
-os.environ['DOMAIN'] = domain
-os.environ['SUB_DOMAIN'] = 'test'
-os.environ['RECORD'] = 'Hello world'
+domain = os.getenv('DOMAIN')
+sub_domain = os.getenv('SUB_DOMAIN')
 
-smbUser = "Username"
-smbPassword = 'Password'
-#smbClientMachinename = ''          # Option
-smbServer = 'DESKTOP-XXXXX'
-smbShare = 'c$'
-smbIp = '10.0.0.0'
-smbPort = '139'                     # Default are 139 or 445
+smbUser = os.getenv('SMB_USER')
+smbPassword = os.getenv('SMB_PASSWORD')
+smbClientMachinename = os.getenv('SMB_CLIENTMACHINENAME')
+smbServer = os.getenv('SMB_SERVER')
+smbShare = os.getenv('SMB_SHARE')
+smbIp = os.getenv('SMB_IP')
+smbPort = os.getenv('SMB_PORT')
+
 path = ''
 filename = 'test.txt'
-os.environ['SMB_USER'] = smbUser
-os.environ['SMB_PASSWORD'] = smbPassword
-os.environ['SMB_SERVER'] = smbServer
-os.environ['SMB_PORT'] = smbPort
-os.environ['SMB_SHARE'] = smbShare
-os.environ['SMB_IP'] = smbIp
-
 #have you imported env variables and install requirements (python and apt)?
 
 os.system('certbot certonly -n --preferred-challenge dns --manual --manual-auth-hook ./auth.py --manual-cleanup-hook ./clean.py --manual-public-ip-logging-ok -d *.%s', domain)
-
-execfile("auth.py")
-
-execfile("clean.py")
 
 conn = SMBConnection(smbUser, smbPassword, smbClientMachinename, smbServer, use_ntlm_v2 = True)
 conn.connect(smbIp, smbPort)
