348fd0f685e5e42ceac7ce4aa6b42c15130df50e
Arch4nge <91314962+Arch4nge@users.noreply.github.com>
2022-01-11 15:31:41 +0100

    Commenting all files and variables

--- auth.py
+++ auth.py
@@ -3,14 +3,15 @@
 import json
 import ovh
 import os
+# Importing python dependances
 
 domain = os.getenv('DOMAIN')
 subDomain = os.getenv('SUB_DOMAIN')
 record = os.getenv('RECORD')
-
 APP_KEY = os.getenv('APP_KEY')
 APP_SECRET = os.getenv('APP_SECRET')
 CONSUMER_KEY = os.getenv('CONSUMER_KEY')
+# LOADING specifics variables environement
 
 client = ovh.Client(
     endpoint='ovh-eu',
@@ -18,6 +19,7 @@ client = ovh.Client(
     application_secret=APP_SECRET,
     consumer_key=CONSUMER_KEY,
 )
+# Establishing connexion with OVH api
 
 id = client.post('/domain/zone/%s/record' %(domain), 
     fieldType='TXT', #// Resource record Name (type: zone.NamedResolutionFieldTypeEnum)
@@ -25,5 +27,7 @@ id = client.post('/domain/zone/%s/record' %(domain),
     target=record, #// Resource record target (type: string)
     ttl='0', #// Resource record ttl (type: long)
 )
+# Create record for certbot verification
 
 os.environ['RECORD_ID'] = id['id']
+# Logging Record ID for deletion after verification
--- main_script.py
+++ main_script.py
@@ -4,7 +4,6 @@ import os
 
 domain = os.getenv('DOMAIN')
 sub_domain = os.getenv('SUB_DOMAIN')
-
 smbUser = os.getenv('SMB_USER')
 smbPassword = os.getenv('SMB_PASSWORD')
 smbClientMachinename = os.getenv('SMB_CLIENTMACHINENAME')
@@ -12,17 +11,28 @@ smbServer = os.getenv('SMB_SERVER')
 smbShare = os.getenv('SMB_SHARE')
 smbIp = os.getenv('SMB_IP')
 smbPort = os.getenv('SMB_PORT')
-
-path = ''
-filename = 'test.txt'
-#have you imported env variables and install requirements (python and apt)?
+sourcePath = os.getenv('SOURCE_PATH')
+destPath = os.getenv('DEST_PATH')
+filename1 = os.getenv('FILENAME1')
+filename2 = os.getenv('FILENAME2')
+# LOADING all variables environement
 
 os.system('certbot certonly -n --preferred-challenge dns --manual --manual-auth-hook ./auth.py --manual-cleanup-hook ./clean.py --manual-public-ip-logging-ok -d *.%s', domain)
+# Generating certificates
 
 conn = SMBConnection(smbUser, smbPassword, smbClientMachinename, smbServer, use_ntlm_v2 = True)
 conn.connect(smbIp, smbPort)
-file2transfer = open(filename,"br")
-value = conn.storeFile(smbShare,path + filename, file2transfer)
-print ("%d Bytes was written", value)
+# Establishing samba connexion
+
+file2transfer = open(sourcePath + '/' + sub_domain + '.' + domain + '/' + filename1,"br")
+value = conn.storeFile(smbShare,destPath + '/' + filename1 , file2transfer)
+print ("%d Bytes was written for file 1", value)
+# Copying first file (fullchain.pem)
+
+file2transfer = open(sourcePath + '/' + sub_domain + '.' + domain + '/' + filename2,"br")
+value = conn.storeFile(smbShare,destPath + '/' + filename2 , file2transfer)
+print ("%d Bytes was written for file 2", value)
+# Copying second file (privkey.pem)
 
 os.system("net rpc restart Apache2.4 --ipaddress %d --user %s%%%s", smbIp, smbUser, smbPassword) #Restart required service
+# Restarting apache to use new certificates
